1757b305ecbbb43e0157223355210d86b7600fa7
woo kyoung <sinwookyoung15@gmail.com>
2025-12-06 18:51:16 +0900

    first commit

---
+++ src/main/java/track_mark/TrackMarkBackendApplication.java
@@ -0,0 +1,13 @@
+package track_mark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackMarkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackMarkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/track_mark/TrackMarkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package track_mark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackMarkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
